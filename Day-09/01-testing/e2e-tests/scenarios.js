12b6a9cdf3ec00ac76f04290d05289df925f5819
tkmagesh <tkmagesh77@gmail.com>
2016-03-11 21:55:16 +0530

    Protractor tests

--- Day-09/01-testing/e2e-tests/scenarios.js
+++ Day-09/01-testing/e2e-tests/scenarios.js
@@ -1,6 +1,7 @@
 'use strict';
 
 /* https://github.com/angular/protractor/blob/master/docs/toc.md */
+/* API Reference - http://angular.github.io/protractor/#/api */
 
 describe('my app', function() {
 
@@ -39,4 +40,29 @@ describe('my app', function() {
     });
 
   });
+
+  describe('greeter', function(){
+    beforeEach(function() {
+      browser.get('index.html#/greet');
+    });
+
+    it("should display the message when greeted", function(){
+      //Arrange
+        var name = 'Magesh',
+            expectedResult = 'Hi Magesh, Have a nice day!';
+
+        var txtName = element(by.model("name"));
+        var btnGreet = element(by.buttonText("Greet")); 
+        var divMessage = element(by.binding("message"));
+      //Act
+       
+        txtName.sendKeys('Magesh');
+        btnGreet.click();
+
+      //Assert
+        var message = divMessage.getText();
+        expect(message).toBe(expectedResult);
+        
+    })
+  })
 });
